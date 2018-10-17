dca7efa698fd03457bc3ae3466c86c928d3c2432
GuziyV <volodyaguziy3@gmail.com>
2018-10-17 12:31:25 +0300

    Implement store item repo

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    interface IUnitOfWork
+    {
+        IRepository<TEntity, TIdentifier> GetRepository<TEntity, TIdentifier>() where TEntity : class;
+        void SaveChanges();
+    }
+}
---
+++ DAL/Models/StoreItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    class StoreItem
+    {
+        public StoreItem(string name, decimal pricePerItem, int count = 0)
+        {
+            Name = name;
+            PricePerItem = pricePerItem;
+            Count = count;
+        }
+
+        public string Name { get; set; }
+
+        public decimal PricePerItem { get; set; }
+
+        public int Count { get; set; }
+    }
+}
