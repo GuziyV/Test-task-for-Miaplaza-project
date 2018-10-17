167ae4a331e9a972d243a3bf4c57282d9a206ce4
GuziyV <volodyaguziy3@gmail.com>
2018-10-17 10:34:47 +0300

    Initial commit(Create project structure)

---
+++ Test task for Miaplaza project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task_for_Miaplaza_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
